ae5a8d37d70da43bb2669277a147d212ae510f09
juank2808 <webjcaf022014@gmail.com>
2020-02-10 20:34:13 +0100

    Example Invoice and relations between its components

---
+++ java/com/springbootdi/app/models/domain/Bill.java
@@ -0,0 +1,42 @@
+package com.springbootdi.app.models.domain;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bill {
+
+	@Value("${bill.description}")
+	private String description;
+	@Autowired
+	private Client client;
+	private List<BillItem> items;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<BillItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<BillItem> items) {
+		this.items = items;
+	}
+
+}
